961eee03384b6a906d7ef87f63a0e2f1f7de754a
billyrigdon <billyrigdoniii@gmail.com>
2021-09-21 11:01:55 -0500

    Rewrote battle logic with delays and messages

--- src/Components/BattleScreen/BattleScreen.jsx
+++ src/Components/BattleScreen/BattleScreen.jsx
@@ -1,18 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import './BattleScreen.scss';
+import React, { Component, Fragment } from "react";
+import "./BattleScreen.scss";
 import Draggable, { DraggableCore } from "react-draggable";
 
-class BattleScreen extends Component {	
+class BattleScreen extends Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return (
+		const moveList = this.props.moves.map((item, index) => (
+			<li
+				className="move"
+				key={item.name}
+				onClick={() => this.props.attack(item)}
+			>
+				{`>`}
+				{item.name}   ----- {item.description}
+			</li>
+		));
 
+		return (
 			<Draggable>
 				<div id="window">
-					
 					<div className="windowBar">
 						<div className="windowButtons minimize"></div>
 						<div className="windowButtons maximize"></div>
@@ -22,30 +31,26 @@ class BattleScreen extends Component {
 					<div id="battle-container">
 						<div id="moves" className="column">
 							<h2>root@gh0st:~$ list_moves.sh</h2>
-							<div onClick={() => { this.props.attack(this.props.moves[0]) }}>>{this.props.moves[0].name} - Deals {this.props.moves[0].damage} Damage</div>
+							<ul>
+								<br />
+								<li>##SANITY: {this.props.sanity}##</li>
+								<br />
+								{moveList}
+							</ul>
+							<br />
+							<h2>{this.props.battleMessage}</h2>
 						</div>
 
 						<div id="enemy" className="column">
-							{this.props.enemyName}
-							<div className="ascii">{`
-\\       /
- \\     /  
-  \\.-./ 
-(o\\^/o)  _   _   _     __
-./ \\.\\ ( )-( )-( ) .-'  '-.
-[-] \\(//  ||   \\/ (   )) '-.
-//-__||__.-\\.       .-'
-(/    ()     \)'-._.-'
-||    ||      \\
-('    ('       ')
-	`}</div>
-							{this.props.enemySanity}
+							<h2>{this.props.enemyName}</h2>
+							<br />
+							##BYTES:{this.props.enemyBytes}##
+							<div className="ascii">{this.props.enemyAscii}</div>
 						</div>
 					</div>
-
 				</div>
 			</Draggable>
-		)
+		);
 	}
 }
 
--- src/Components/KernelPanicContainer/KernelPanicContainer.jsx
+++ src/Components/KernelPanicContainer/KernelPanicContainer.jsx
@@ -1,146 +1,215 @@
-import React, { Component, Fragment } from 'react';
-import KernelPanic from '../../KernelPanic';
-import BattleScreen from '../BattleScreen/BattleScreen';
-import './KernelPanic.css'
-
+import React, { Component, Fragment } from "react";
+import KernelPanic from "../../KernelPanic";
+import BattleScreen from "../BattleScreen/BattleScreen";
+import "./KernelPanic.css";
 
 class KernelPanicContainer extends Component {
 	constructor(props) {
 		super(props);
-	//Bind attack function so state can update from child component
+		//Bind attack function so state can update from child component
 		this.attack = this.attack.bind(this);
 	}
 	state = {
-		// Player stats
+		// Player properties
 		sanity: 100,
 		typingSpeed: 5,
 		brainPower: 5,
-		moves: [{
-			name: "Hello World",
-			damage: 1,
-			heal: 0,
-			type: "whiteHat"
-		}],
-		items: [{
-			name: "Risperidone",
-			sanity: 50,
-			typingSpeed: 0,
-			brainPower: 0,
-			quantity: 5
-		}],
-		
+		moves: [
+			{
+				name: "console.log('Hello World');",
+				damage: 1,
+				heal: 0,
+				type: "whiteHat",
+				description: "Damage:1",
+			},
+			{
+				name: "Brute Force",
+				damage: 2,
+				heal: 0,
+				type: "blackHat",
+				description: "Damage: 2",
+			},
+			{
+				name: "Pray",
+				damage: 0,
+				heal: 10,
+				type: "spiritual",
+				description: "Raises Sanity by 10",
+			},
+		],
+		items: [
+			{
+				name: "Risperidone",
+				sanity: 50,
+				typingSpeed: 0,
+				brainPower: 0,
+				quantity: 5,
+			},
+		],
+
 		//Enemy Stats
 		enemyName: "eXpl01t.exe",
-		enemySanity: 20,
-		enemySpeed: 3,
-		enemyPower: 3,
+		enemyBytes: 100,
+		enemySpeed: 10,
+		enemyPower: 50,
 		enemyType: "bug",
+		enemyAscii: `
+		\\       /
+		 \\     /  
+		  \\.-./ 
+		  (o\\^/o)  _   _   _     __
+		  ./ \\.\\ ( )-( )-( ) .-'  '-.
+		  [-] \\(//  ||   \\/ (   )) '-.
+		  //-__||__.-\\.       .-'
+		  (/    ()     \)'-._.-'
+		  ||    ||      \\
+		  ('    ('       ')`,
 
 		//Opens battle screen if true
 		battleOpen: true,
-	}
+		//Message that is displayed in battle
+		battleMessage: "",
+	};
 
-	attack(move) {		
-	//If your speed is greater than enemy, your move goes first
-		if (this.state.enemySpeed < this.state.typingSpeed) {
-		//Check if move does damage
-			if (move.damage > 0) {
-			//Check if move would kill enemy. If it does, attack goes through and battle ends
-				if (this.state.enemySanity - (this.state.brainPower + move.damage) <= 0) {
-					this.setState({
-						enemySanity: this.state.enemySanity - (this.state.brainPower + move.damage),
-						sanity: this.state.sanity + move.heal,
-						battleOpen: false
-					})
-				} else {
-					this.setState({
-						enemySanity: this.state.enemySanity - (this.state.brainPower + move.damage),
-						sanity: this.state.sanity + move.heal
-					})
-				}
-			//If enemy sanity is greater than 0, player takes damage
-			//If enemy sanity goes to 0 or below, the battle ends
-				if (this.state.enemySanity > 0) {
-					this.setState({
-						sanity: this.state.sanity - this.state.enemyPower
-					})
-				} else if (this.state.enemySanity <= 0) {
-					this.setState({
-						battleOpen: false
-					})
-				}
-		//If the move doesn't have a damage value, no damage is dealt but user still heals
-			} else if (move.damage <= 0) {
-				this.setState({
-					sanity: this.state.sanity + move.heal
-				})
-				if (this.state.enemySanity > 0) {
-					this.setState({
-						sanity: this.state.sanity - this.state.enemyPower
-					})
-				} 
-			}
-	//If enemy speed is greater, enemy deals damage first
-		} else if (this.state.enemySpeed >= this.state.typingSpeed) {
-			this.setState({
-				sanity: this.state.sanity - this.state.enemyPower
-			})
-		//If enemy damage didn't drain your sanity, your attack will succeed
-			if (this.state.sanity > 0) {
-				if (move.damage > 0) {
-					this.setState({
-						enemySanity: this.state.enemySanity - (this.state.brainPower + move.damage),
-						sanity: this.state.sanity + move.heal
-					})
-				} else if (move.damage <= 0) {
-					this.setState({
-						sanity: this.state.sanity + move.heal
-					})
-				}
-
-			//End battle if enemy sanity < 0
-				if (this.state.enemySanity <= 0) {
-					this.setState({
-						battleOpen: false
-					})
-				}
-			}
-		}
+	attack(move) {
+		const enemSpeed = this.state.enemySpeed;
+		const speed = this.state.typingSpeed;
+		const pow = this.state.brainPower;
+		const enemPow = this.state.enemyPower;
 		
+		//Set how much damage your move will do
+		let moveDamage = 0
+		if (move.damage > 0) {
+			moveDamage = move.damage + pow;
+		} 
+
+		//Compare speeds to decide who goes first
+		if (enemSpeed < speed) {
+			//If move does damage, deal that damage
+			if (move.damage > 0) {
+				this.setState({
+					enemyBytes: this.state.enemyBytes - (move.damage + pow),
+				})
+			}
+			//If move heals, raise sanity. Update battle message
+			this.setState({
+				sanity: this.state.sanity + (move.heal),
+				battleMessage: `Dealt ${moveDamage} Byte Damage and gained ${move.heal} Sanity`
+			});
+			//If enemy is not defeated by move, sanity is lost. Battle message updated 
+			if (this.state.enemyBytes - (move.damage + pow) > 0) {
+				setTimeout(() => {
+					this.setState({
+						sanity: this.state.sanity - enemPow,
+						battleMessage: `Lost ${enemPow} sanity`
+					})
+					setTimeout(() => {
+						this.setState({
+							battleMessage: ""
+						})
+					},1300)
+				}, 1300)
+			} else {
+				setTimeout(() => {
+					this.setState({
+						battleMessage: `You've defeated ${this.state.enemyName}`
+					})
+				}, 1300);
+				//End battle if enemy is defeated
+				setTimeout(() => {
+					this.setState({
+						battleOpen: false
+					})
+				},2600)
+			}
+			
+		} else if (enemSpeed > speed) {
+			this.setState({
+				sanity: this.state.sanity - enemPow,
+				battleMessage: `You lost ${enemPow} sanity`
+			});
+			if (this.state.sanity - enemPow > 0) {
+				setTimeout(() => {
+					if (move.damage) {
+						this.setState({
+							enemyBytes: this.state.enemyBytes - (move.damage + pow)
+						})
+					}
+					this.setState({
+						sanity: this.state.sanity + (move.heal),
+						battleMessage: `Dealt ${moveDamage} Byte Damage and gained ${move.heal} Sanity`
+					})
+					if (this.state.enemyBytes - (move.damage + speed) <= 0) {
+						setTimeout(() => {
+							this.setState({
+								battleMessage: `You've defeated ${this.state.enemyName}`
+							})
+							setTimeout(() => {
+								this.setState({
+									battleOpen: false,
+									battleMessage: ""
+								})
+							}, 1300)
+						},2600)
+					} else {
+						setTimeout(() => {
+							this.setState({
+								battleMessage: ""
+							})
+						},1300)
+					}
+				}, 1300)
+
+			} else {
+				setTimeout(() => {
+					this.setState({
+						battleMessage: `You've officially gone insane`
+					})
+				}, 1300)
+				setTimeout(() => {
+					this.setState({
+						battleMessage: "",
+						battleOpen: false
+					})
+				},2600)
+			} 
+		}
 	}
+
 
 	componentDidMount() {
-	//Checks localstorage for moves and sets state accordingly
-	//If there's no localstorage set, it uses the default moves and sets localstorage 
+		//Checks localstorage for moves and sets state accordingly
+		//If there's no localstorage set, it uses the default moves and sets localstorage
 		if (localStorage.getItem("moves")) {
 			this.setState({
-				moves: JSON.parse(localStorage.getItem("moves"))
-			})
+				moves: JSON.parse(localStorage.getItem("moves")),
+			});
 		} else if (!localStorage.getItem("moves")) {
 			localStorage.setItem("moves", JSON.stringify(this.state.moves));
 		}
 
-	//Same thing as above, but with items
+		//Same thing as above, but with items
 		if (localStorage.getItem("items")) {
 			this.setState({
-				items: JSON.parse(localStorage.getItem("items"))
-			})
+				items: JSON.parse(localStorage.getItem("items")),
+			});
 		} else if (!localStorage.getItem("items")) {
 			localStorage.setItem("items", JSON.stringify(this.state.items));
 		}
 
-
-	//Creates Game
+		//Creates Game
 		this.game = new KernelPanic(this);
 	}
 
 	render() {
 		return (
 			<Fragment>
-				{this.state.battleOpen && <BattleScreen attack={this.attack} {...this.state} />}
+				{this.state.battleOpen && (
+					<BattleScreen attack={this.attack} {...this.state} />
+				)}
 				<div id="gameContainer"></div>
 			</Fragment>
-		)
+		);
 	}
 }
 
